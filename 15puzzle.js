84e70ece8cfd6bcad31e185c481fca32c7527a46
DouG Molidor <doug@creativelift.net>
2011-09-29 21:13:14 -0700

    Tiles drag, to where, I have no idea

--- 15puzzle.js
+++ 15puzzle.js
@@ -7,10 +7,22 @@
 	    return a;
 	}
 	
+	function hasClass(el, selector) {
+		var className = " " + selector + " ";
+		return (" " + el.className + " ").replace(/[\n\t]/g, " ").indexOf(className) > -1 ? true : false;
+	}
+	
+	function addClass(el, selector) {
+		if (!hasClass(el, selector)) el.setAttribute( "class", el.className + " " + selector);
+		return el;
+	}
+	
 	function Puzzle(_) {
 		this.el = _;
-		
-		this.cellsNum = 16;
+				
+		this.tileNums = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,false];
+
+		_.setAttribute("class","puzzle");
 		
 		_.childrenAry = function() {
 			arr=[];
@@ -18,13 +30,15 @@
 			return arr;
 		}
 		
-		for( i=0; i<this.cellsNum; i++ ) {
+		for( i=0; i<this.tileNums.length; i++ ) {
 			var cell = new Cell(document.createElement("div"));
+			var tile = new Tile(document.createElement("div"));
+			if (this.tileNums[i]) {
+				tile.el.appendChild(document.createTextNode(this.tileNums[i]));
+				cell.el.appendChild(tile.el);
+			}
 			_.appendChild(cell.el);
-			console.log(cell.el.offsetWidth);
 		}
-		_.X = "101";
-		_.setAttribute("class","puzzle");
 		
 		console.log(this);
 	}
@@ -39,7 +53,11 @@
 		_.setAttribute("class","cell");
 		
 		_.addEventListener("mouseover", function(e){ 
-			var eligible = this.parentElement.childrenAry().filter(function(o, i){
+			_.getEligibleCells();
+		});
+		
+		_.getEligibleCells = function(){
+			var eligible = _.parentElement.childrenAry().filter(function(o, i){
 				o.setAttribute("class","cell");
 				var eligibleHorz = (o.offsetLeft == _.X() - _.W() 
 					|| o.offsetLeft == _.X() + _.W())
@@ -50,14 +68,33 @@
 				return eligibleHorz || eligibleVert;
 			});
 			eligible.map( function(o){
-				o.setAttribute("class","cell eligible")
+				o.hasChildNodes() ? addClass(o,"eligible") : addClass(o,"eligible blank");
 			})
-		});
+			return eligible;
+		}
 		
 		_.X = function(){ return _.offsetLeft; };
 		_.Y = function(){ return _.offsetTop; };
 		_.H = function(){ return _.offsetHeight; };
 		_.W = function(){ return _.offsetWidth; };
+	}
+	
+	function Tile(_) {
+		this.el = _;
+		
+		_.setAttribute("class","tile");
+		
+		_.addEventListener("mouseover", function(e){ 
+			var blankCell = this.parentElement.getEligibleCells().filter(function(o, i){
+				return hasClass(o, "blank") ? true : false;
+			})[0];
+			if(blankCell) {
+				addClass(this, "draggable");
+				this.draggable = true;
+			}
+		
+		});
+		
 	}
 	
 	var puz = new Puzzle(document.createElement("div"));
